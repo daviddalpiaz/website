92e0a9392a7187238ebb411912bc17708345867f
daviddalpiaz <dalpiaz2@illinois.edu>
2018-08-10 19:38:46 -0400

    starting to think about updates

---
+++ README.md
@@ -0,0 +1,4 @@
+# website
+
+- Currently deployed via [Netlify](netlify.com)
+- Favicon generated via [Real Favicon Generator](http://realfavicongenerator.net/)
--- make_logo.R
+++ make_logo.R
@@ -1,6 +1,6 @@
-thickness = 5
+thickness = 10
 dev.off()
-par(bg=NA) 
+par(bg = NA) 
 plot(
   1,
   type = "n",
@@ -11,8 +11,6 @@ plot(
   axes = FALSE,
   frame.plot = FALSE
 )
-curve(dnorm(x, mean = 4, sd = sqrt(2)), col = "darkorange", add = TRUE, lwd = thickness)
-curve(dnorm(x, mean = 5, sd = sqrt(1)), col = "dodgerblue", add = TRUE, lwd = thickness)
-curve(dnorm(x, mean = 6, sd = sqrt(0.5)), col = "grey", add = TRUE, lwd = thickness)
-
-# http://realfavicongenerator.net/
+curve(dnorm(x, mean = 6, sd = sqrt(0.5)), col = "#b3b3b3", add = TRUE, lwd = thickness)
+curve(dnorm(x, mean = 5, sd = sqrt(1)), col = "#666666", add = TRUE, lwd = thickness)
+curve(dnorm(x, mean = 4, sd = sqrt(2)), col = "#bb0000", add = TRUE, lwd = thickness)
