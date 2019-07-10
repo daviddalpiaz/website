9fd66359d9d9f215a69371be9db4d7361db22828
daviddalpiaz <dalpiaz2@illinois.edu>
2019-07-10 09:04:08 -0500

    scripting image creation

--- make_logo.R
+++ make_logo.R
@@ -1,19 +1,26 @@
-thickness = 6
+make_normal_plot = function(thickness = 7) {
+  par(bg = NA, mar = c(0, 0, 0, 0)) 
+  plot(
+    1,
+    type = "n",
+    xlab = "",
+    ylab = "",
+    xlim = c(-1, 10),
+    ylim = c(0, 0.56),
+    axes = FALSE,
+    frame.plot = FALSE
+  )
+  curve(dnorm(x, mean = 6, sd = sqrt(0.5)), col = "#666666", add = TRUE, lwd = thickness, n = 10000)
+  curve(dnorm(x, mean = 5, sd = sqrt(1)), col = "#888888", add = TRUE, lwd = thickness, n = 10000)
+  curve(dnorm(x, mean = 4, sd = sqrt(2)), col = "#999999", add = TRUE, lwd = thickness, n = 10000)
+}
+
 dev.off()
-par(bg = NA) 
-plot(
-  1,
-  type = "n",
-  xlab = "",
-  ylab = "",
-  xlim = c(0, 10),
-  ylim = c(0, 0.61),
-  axes = FALSE,
-  frame.plot = FALSE
-)
-curve(dnorm(x, mean = 6, sd = sqrt(0.5)), col = "#666666", add = TRUE, lwd = thickness)
-curve(dnorm(x, mean = 5, sd = sqrt(1)), col = "#888888", add = TRUE, lwd = thickness)
-curve(dnorm(x, mean = 4, sd = sqrt(2)), col = "#999999", add = TRUE, lwd = thickness)
+png("public/wordmark.png", width = 1000, height = 400)
+make_normal_plot()
+dev.off()
+
+
 
 
 
